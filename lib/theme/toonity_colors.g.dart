0534cad86e4d5fe4feab1eafd566562c58565f27
Parabeac-Nest <admin@parabeac.com>
2023-03-20 20:59:21 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/toonity_colors.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ToonityColors {
+
+static const Color primaryWhite = Color(0xffffffff);
+
+static const Color secondaryGray = Color(0xff9d9d9d);
+
+static const Color primaryBlack = Color(0xff424242);
+
+static const Color primaryMain = Color(0xffa2b2fc);
+
+static const Color carbonNeutral300 = Color(0xffd6d9dd);
+
+static const Color carbonDarkest = Color(0xff24262b);
+}
